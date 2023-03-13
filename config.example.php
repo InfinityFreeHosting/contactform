20404edc14e93e52e35ddd8b9f1d253b6bf67de4
Hans <git@xvh.nl>
2023-03-13 14:18:19 +0100

    Add character encoding and language settings (#12)

--- config.example.php
+++ config.example.php
@@ -35,3 +35,11 @@ define('CONTACTFORM_PHPMAILER_DEBUG_LEVEL', 0);
 // Which SMTP port and encryption type to use. The default is probably fine for most use cases.
 define('CONTACTFORM_SMTP_PORT', 587);
 define('CONTACTFORM_SMTP_ENCRYPTION', 'tls');
+
+// Character encoding settings. The default is probably fine for most use cases.
+define('CONTACTFORM_MAIL_CHARSET', 'iso-8859-1'); // Can be: us-ascii, iso-8859-1, utf-8. Default: iso-8859-1.
+define('CONTACTFORM_MAIL_ENCODING', '8bit'); // Can be: 7bit, 8bit, base64, binary, quoted-printable. Default: 8bit.
+
+// The language used for error message and the like.
+// Supports 2 letter language codes, e.g. en, fr, es, cn. Default: en.
+define('CONTACTFORM_LANGUAGE', 'en');
